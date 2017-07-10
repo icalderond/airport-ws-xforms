e5bd67e8ed87edf19bb08c7ec221e2db8fed7e5a
icalderond <icalderond@outlook.com>
2017-07-10 07:03:24 -0500

    Consumo de webservice corrección

--- Droid/Service/SoapService.cs
+++ Droid/Service/SoapService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using airports_country.Model;
 
 namespace airports_country.Droid.Service
@@ -9,11 +11,24 @@ namespace airports_country.Droid.Service
     public class SoapService : IAirportService
     {
         WSAeropuerto.airport service;
+        public SoapService()
+        {
+            service = new WSAeropuerto.airport();
+        }
         public List<Aeropuerto> GetDataAsync(string _country)
         {
             var result = service.GetAirportInformationByCountry(_country);
-            var lista = from item in result.ToList()
-                        select item;
+            XDocument doc;
+
+            using (var s = new StringReader(result))
+                doc = XDocument.Load(result);
+
+            XNamespace general = "http://www.webserviceX.NET";
+
+            //XNameSpace n;
+            var lista = from item in doc.Descendants(general + "newdataset")
+                                        .Descendants(general + "table")
+                        select item.Element(general + "cityorairportname").Value;
             return new List<Aeropuerto>();
         }
     }
